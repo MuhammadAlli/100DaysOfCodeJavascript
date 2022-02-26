3c878185558356bf010805400048933d55e5fd51
MuhammadAlli <97398067+MuhammadAlli@users.noreply.github.com>
2022-02-26 12:55:30 +0600

    Delete day-1 directory

--- day-1/app.js
+++
@@ -1,9 +0,0 @@
-let usd = document.querySelector("#usd");
-let bdt = document.querySelector("#bdt");
-let btn = document.querySelector("#btn");
-
-btn.addEventListener("click", BDT);
-
-function BDT() {
-    bdt.innerText = usd.value * 85.96;
-}
